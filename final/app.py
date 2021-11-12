2e3cf613d4c97697ff33b894078e946bf47c7924
Manveer Sadhal <manveer.shopping@gmail.com>
2021-11-11 17:59:59 -0800

    update app and notebooks. add slides and screenshots

--- final/app.py
+++ final/app.py
@@ -3,17 +3,14 @@ import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
 import datetime as dt
-import plotly.express as px
 from wordcloud import WordCloud
-from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 import matplotlib.pyplot as plt
+from sklearn.decomposition import TruncatedSVD, NMF
+from sklearn.feature_extraction.text import TfidfVectorizer, ENGLISH_STOP_WORDS
 
-from sklearn.decomposition import TruncatedSVD, NMF, LatentDirichletAllocation
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-
+# import plotly.express as px
 # import pyLDAvis
 # import pyLDAvis.sklearn
-
 
 def display_wordcloud(text):
     wc_text = ' '.join(text)
@@ -21,7 +18,9 @@ def display_wordcloud(text):
                       collocations=True).generate(wc_text)
     fig, ax = plt.subplots()
     ax.imshow(wordcloud, interpolation='bilinear')
+    ax.axis('off')
     st.pyplot(fig)
+
 
 def display_topics(model, feature_names, no_top_words):
     output_list = []
@@ -32,104 +31,132 @@ def display_topics(model, feature_names, no_top_words):
     return ''.join(output_list)
 
 
+def create_lsa_tfidf(docs, num_topics, stop_words, min_df, max_df, max_features):
+    tfidf = TfidfVectorizer(stop_words=stop_words, min_df=min_df, max_df=max_df, max_features=max_features)
+    df_fit = tfidf.fit_transform(docs)
+    tfidf_df = pd.DataFrame(df_fit.toarray(), columns=tfidf.get_feature_names_out())
+    lsa = TruncatedSVD(num_topics)
+    doc_topic = lsa.fit_transform(tfidf_df)
+    return lsa, tfidf
+
+
+def create_nmf_model(docs, num_topics, stop_words, min_df, max_df, max_features):
+    vectorizer = TfidfVectorizer(stop_words=stop_words, max_features=max_features, min_df=min_df, max_df=max_df)
+    doc_word = vectorizer.fit_transform(docs)
+    nmf_model = NMF(num_topics)
+    doc_topic = nmf_model.fit_transform(doc_word)
+    return nmf_model, vectorizer
+
+
 my_stop_words = ['like', 'likes', 'just', 'shares', 'guys', 'click', 'x200b', 'going', 'went', 'stock', 'stocks', 'market', 'know', 'link', 'think',\
                 'want', 'ape', 'apes', 'company', 'companies', 'earnings', 'short', 'shit', 'fucking', 'million', 'billion', 'revenue', 'let', 'got',\
                  'fuck', 'fucking', 'fucked', 'retard', 'retards', 'retarded', 'people', 'mods', 'sub', 'post', 'did', 'ass', 'make', 'maybe',\
-                 'tell', 'getting', 'autists', 'autistic', 'tendies', 'listen', 'remember', 'talking', 'buy', 'hold', 'sell', 'good', 'new', 'time', 'today',\
+                 'tell', 'getting', 'autist', 'autists', 'autistic', 'tendies', 'listen', 'remember', 'talking', 'buy', 'hold', 'sell', 'good', 'new', 'time', 'today',\
                 'week', 'use', 'invest', 'bought', 'need', 'day', 'really', 'right', 'high', 'low', 'big', 'need', 'right', 'money', 'cash',\
                 'lot', 'need', 'does', 'day', 'right', 'way', 'price', 'trade', 'trading', 'trades', 'calls', 'option', 'options', 'short',\
                  'shorts', 'shorted', 'long', 'share', 'thoughts', 'missed', 'come', 'coming', 'doing', 'thing', 'trying', 'say', 'feel', 'goes', 'actually', 'porn',\
                  'meme', 'squeeze', 'holding', 'buying', 'wsb', 'days', 'lose', 'look', 'looking', 'knows', 'believe', 'possibly', 'said', 'nbsp',\
                 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december', 'thinking',\
                 'month', 'year', 'years', 'probably', 'better', 'tl', 'dr', 'likely', 'sure', 'great', 'months', 'things', 'means', 'started', 'business',\
-                 'industry', 'based', 'pretty', 'total', 'best', 'real', 'currently', 'play', 'kind', 'little']
+                 'industry', 'based', 'pretty', 'total', 'best', 'real', 'currently', 'play', 'kind', 'little', 'yeah', 'nbsp', 'hello', 'hey', 'dd', 'tldr']
 stop_words = ENGLISH_STOP_WORDS.union(my_stop_words)
 
 #create db connection
 engine = create_engine('sqlite:///data.db')
+periods=35
+date_ranges = [date.date() for date in pd.date_range('2019-01-01', periods=periods, freq="MS")]
 
-#get user input for dates to display
-st.title('WallStreetBets Topic Comparison')
-st.write("Word Cloud Comparison")
-date_ranges = [date.date() for date in pd.date_range('2019-01-01', periods=35, freq="MS")]
+st.sidebar.title("Menu")
+display_option = st.sidebar.radio("Select Module:", options=['Word Clouds', 'Topic Modeling'])
 
-col1, col2 = st.beta_columns(2)
+if display_option == 'Word Clouds':
+    #get user input for dates to display
+    st.title('WallStreetBets Keywords')
+    st.markdown("### Word Cloud Comparison")
 
-with col1:
-    left_date = st.selectbox('First Date', date_ranges)
+    left_date_index = periods - 20
+    right_date_index = periods - 1
 
-with col2:
-    right_date = st.selectbox('Second Date', date_ranges)
+    if st.button('Click Here to Randomly Select Two Months to Compare'):
+        left_date_index = np.random.choice(periods - 1)
+        right_date_index = np.random.choice(periods - 1)
+        while left_date_index == right_date_index:
+            right_date_index = np.random.choice(periods - 1)
 
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        left_date = st.selectbox('First Month', date_ranges, format_func=lambda x: x.strftime('%B %Y'), index=left_date_index)
 
-# if date1 != date2, query db and create wordclouds
+    with col2:
+        right_date = st.selectbox('Second Month', date_ranges, format_func=lambda x: x.strftime('%B %Y'), index=right_date_index)
 
-table = 'submissions'
-start_date = left_date
-end_date = left_date + dt.timedelta(days=7)
-sql = f"""
-    SELECT docs_clean
-    FROM {table}
-    WHERE date BETWEEN '{start_date}' AND '{end_date}';
-    """
+    table = 'submissions'
+    start_date = left_date
+    end_date = left_date + dt.timedelta(days=7)
+    sql = f"""
+        SELECT docs_clean
+        FROM {table}
+        WHERE date BETWEEN '{start_date}' AND '{end_date}';
+        """
 
-left_df = pd.read_sql(sql, engine)
+    left_df = pd.read_sql(sql, engine)
 
-table = 'submissions'
-start_date = right_date
-end_date = right_date + dt.timedelta(days=7)
-sql = f"""
-    SELECT docs_clean
-    FROM {table}
-    WHERE date BETWEEN '{start_date}' AND '{end_date}';
-    """
+    table = 'submissions'
+    start_date = right_date
+    end_date = right_date + dt.timedelta(days=7)
+    sql = f"""
+        SELECT docs_clean
+        FROM {table}
+        WHERE date BETWEEN '{start_date}' AND '{end_date}';
+        """
 
-right_df = pd.read_sql(sql, engine)
+    right_df = pd.read_sql(sql, engine)
 
-with col1:
-    display_wordcloud(left_df['docs_clean'])
-with col2:
-    display_wordcloud(right_df['docs_clean'])
+    with col1:
+        display_wordcloud(left_df['docs_clean'])
+    with col2:
+        display_wordcloud(right_df['docs_clean'])
 
-# min_df = 0.05
-# max_df = 0.8
-# max_features = len(left_df['docs_clean']) // 2
-# num_topics = 5
-# words_to_display = 7
+else:
+    st.title("WallStreetBets Topic Modeling")
+    st.markdown("#### Choose Month, Model Type, and Model Parameters Below")
 
-st.sidebar.title("LSA Topic Modeling")
-topic_model_date = st.sidebar.selectbox('Month to Topic Model', date_ranges)
+    topic_model_date = st.selectbox('Month to Model', date_ranges, format_func=lambda x: x.strftime('%B %Y'), index=periods - 1)
+    
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        model_type = st.selectbox("Type of Model", ['NMF', 'LSA'])    
 
-table = 'submissions'
-start_date = topic_model_date
-end_date = topic_model_date + dt.timedelta(days=7)
-sql = f"""
-    SELECT docs_clean
-    FROM {table}
-    WHERE date BETWEEN '{start_date}' AND '{end_date}';
-    """
+    table = 'submissions'
+    start_date = topic_model_date
+    end_date = topic_model_date + dt.timedelta(days=7)
+    sql = f"""
+        SELECT docs_clean
+        FROM {table}
+        WHERE date BETWEEN '{start_date}' AND '{end_date}';
+        """
 
-model_df = pd.read_sql(sql, engine)
+    model_df = pd.read_sql(sql, engine)
 
-min_df = st.sidebar.number_input('Minimum Document Frequency', min_value=0.0, max_value=1.0, value=0.05)
-max_df = st.sidebar.number_input("Maximum Document Frequency", min_value=0.0, max_value=1.0, value=0.8)
-max_features = st.sidebar.number_input("Maximum Features", min_value=1, max_value=int(len(model_df['docs_clean'])), value=int(len(model_df['docs_clean']) // 2))
-num_topics = st.sidebar.number_input("Number of Topics", min_value=1, max_value=30, value=5)
-words_to_display = st.sidebar.number_input("Words per topic to display", min_value=3, max_value=10, value=5)
+    default_max_features = len(model_df['docs_clean']) // 4 * 3
+    max_features_max = len(model_df['docs_clean'])
+    default_num_topics = 7
 
+    with col1:
+        min_df = st.number_input('Minimum Document Frequency', min_value=0.0, max_value=1.0, value=0.0)
+        max_df = st.number_input("Maximum Document Frequency", min_value=0.0, max_value=1.0, value=0.9)
+    
+    with col2:
+        max_features = st.number_input("Maximum Features", format='%i', min_value=1, max_value=max_features_max, value=default_max_features, step=1)
+        num_topics = st.number_input("Number of Topics", min_value=1, max_value=30, value=default_num_topics)
+        words_to_display = st.number_input("Words per Topic to Display", min_value=3, max_value=10, value=5)
 
-tfidf = TfidfVectorizer(stop_words=stop_words, min_df=min_df, max_df=max_df, max_features=max_features)
-df_fit = tfidf.fit_transform(model_df['docs_clean'])
-tfidf_df = pd.DataFrame(df_fit.toarray(), columns=tfidf.get_feature_names_out())
-lsa = TruncatedSVD(num_topics)
-doc_topic = lsa.fit_transform(tfidf_df)
-
-st.write("Model Results:")
-st.markdown(display_topics(lsa, tfidf.get_feature_names_out(), words_to_display), unsafe_allow_html=True)
-
-# tf_vectorizer = CountVectorizer(stop_words=stop_words, max_df=0.9, max_features=1000)
-# dtm_tf = tf_vectorizer.fit_transform(left_df['docs_clean'])
-# lda_tf = LatentDirichletAllocation(n_components=5, random_state=137)
-# lda_tf.fit(dtm_tf)
-# pyLDA_prepared = pyLDAvis.sklearn.prepare(lda_tf, dtm_tf, tf_vectorizer)
-# st.markdown(pyLDAvis.prepared_data_to_html(pyLDA_prepared, template_type='simple'), unsafe_allow_html=True)
+    if model_type == 'LSA':
+        model, vectorizer = create_lsa_tfidf(docs=model_df['docs_clean'], num_topics=num_topics, stop_words=stop_words, min_df=min_df, max_df=max_df, max_features=max_features)
+    else:
+        model, vectorizer = create_nmf_model(docs=model_df['docs_clean'], num_topics=num_topics, stop_words=stop_words, min_df=min_df, max_df=max_df, max_features=max_features)
+    
+    st.markdown("### Model Results:")
+    st.markdown(display_topics(model, vectorizer.get_feature_names_out(), words_to_display), unsafe_allow_html=True)
